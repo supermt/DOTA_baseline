cbfce77e8980482bf0f7b7e790c1e78b7fbf9d22
supermt <supertempler@gmail.com>
2020-12-01 20:52:14 +0800

    Rename directories and the running command shell script

--- motivation_model/mixgraph_test/running_command.sh
+++ motivation_model/mixgraph_test/running_command.sh
@@ -1,1 +1,11 @@
-nohup python3 mixgraph_testing.py > /home/supermt/mixgraph_test/python.log 2>&1 &
+needed_arguments=1
+if [ "$#" -le "$needed_arguments" ];
+then
+  echo "No enough arguments, missing workdir at $needed_arguments , received only $@"
+else
+  echo "Store the python log at $1"
+  nohup python3 mixgraph_testing.py > /home/supermt/mixgraph_test/python.log 2>&1 &
+  cp /home/supermt/mixgraph_test/python.log $1
+fi
+
+
