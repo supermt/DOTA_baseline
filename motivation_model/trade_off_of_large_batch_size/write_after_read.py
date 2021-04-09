8196d815d5b092e8ca7a590f6681ad11bbdd54c0
supermt <supertempler@gmail.com>
2021-04-09 21:34:11 +0800

    add the results and script of trade off analysis

---
+++ motivation_model/trade_off_of_large_batch_size/write_after_read.py
@@ -0,0 +1,41 @@
+import os
+import sys
+
+work_path = os.getcwd()
+os.chdir("../")
+print(os.getcwd())
+sys.path.insert(0, '.')
+
+from db_bench_option import DEFAULT_DB_BENCH
+from db_bench_option import load_config_file
+from db_bench_option import set_parameters_to_env
+from db_bench_runner import DB_launcher
+from db_bench_runner import reset_CPUs
+from parameter_generator import HardwareEnvironment
+from parameter_generator import StorageMaterial
+from db_bench_runner import clean_cgroup
+
+os.chdir(work_path)
+if __name__ == '__main__':
+    env = HardwareEnvironment()
+    parameter_dict = load_config_file('config.json')
+    set_parameters_to_env(parameter_dict, env)
+
+    result_dir = "/home/jinghuan/trade_off_analysis"
+
+    runner = DB_launcher(
+        env, result_dir, db_bench=DEFAULT_DB_BENCH, extend_options={
+            "num":"26843545",
+            "report_interval_seconds": 1,
+            "mutable_compaction_thread_prior": "false",
+            "duration": 3600,
+            "benchmarks":"fillrandom,readrandom,stats",
+            "statistics":"true",
+            "reads":"100000"
+	    # "record_level_files": "true",
+            # "change_points": "[{target_file_size_base,134217728,600},{write_buffer_size,134217728,600}]",
+            #"DOTA_enabled":"true",
+        })
+    runner.run()
+    reset_CPUs()
+    clean_cgroup()
