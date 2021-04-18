7ebadaf6dcd207c3056855e8718b4f9b3d73d0aa
supermt <supertempler@gmail.com>
2021-04-18 23:41:13 +0900

    fixed a bug in rate_limiting_motivation.py, caused by the quota

--- motivation_model/introduction_motivation/rate_limiting_motivation.py
+++ motivation_model/introduction_motivation/rate_limiting_motivation.py
@@ -21,12 +21,12 @@ if __name__ == '__main__':
     parameter_dict = load_config_file('config.json')
     set_parameters_to_env(parameter_dict, env)
 
-    result_dir = "/home/jinghuan/introduction_motivation/"
+    result_dir = "/home/supermt/introduction_motivation/"
     write_in_rates = parameter_dict["benchmark_write_rate_limit"]
 
     for write_in_rate in write_in_rates:    
         runner = DB_launcher(
-            env, result_dir+str(write_in_rate)", db_bench=DEFAULT_DB_BENCH, extend_options={
+            env, result_dir+str(write_in_rate), db_bench=DEFAULT_DB_BENCH, extend_options={
                 "report_interval_seconds": 1,
                 "duration": 1200,
                 "benchmarks":"fillrandom,stats",
