23f890bc8919e1e26dcddf8f9482ddc295c21721
supermt <supertempler@gmail.com>
2021-04-25 17:13:20 +0900

    add the result of comparing the different CPU number's influence, aims to ensure the influence is caused by thread number, not CPU number

---
+++ motivation_model/cpu_or_thread/12threads_12cpu.py
@@ -0,0 +1,41 @@
+# default path parameter
+import os
+import sys
+
+work_path = os.getcwd()
+os.chdir("../")
+print(os.getcwd())
+sys.path.insert(0, '.')
+
+from db_bench_option import DEFAULT_DB_BENCH
+from db_bench_option import load_config_file
+from db_bench_option import set_parameters_to_env
+from db_bench_runner import DB_launcher
+from db_bench_runner import reset_CPUs
+from parameter_generator import HardwareEnvironment
+from parameter_generator import StorageMaterial
+from db_bench_runner import clean_cgroup
+from db_bench_runner import restrict_cpus
+
+os.chdir(work_path)
+if __name__ == '__main__':
+    env = HardwareEnvironment()
+    parameter_dict = load_config_file('config.json')
+    set_parameters_to_env(parameter_dict, env)
+
+    result_dir = "/home/supermt/turn_off_cpu"
+    restrict_cpus(12,1) 
+    runner = DB_launcher(
+        env, result_dir, db_bench=DEFAULT_DB_BENCH, extend_options={
+            "benchmarks":"fillrandom,stats",
+            "report_interval_seconds": 1,
+            "mutable_compaction_thread_prior": "false",
+	    # "record_level_files": "true",
+            # "change_points": "[{target_file_size_base,134217728,600},{write_buffer_size,134217728,600}]",
+            #"DOTA_enabled":"true",
+            "statistics":"true",
+            "duration":"1200"
+        })
+    runner.run()
+    reset_CPUs()
+    clean_cgroup()
