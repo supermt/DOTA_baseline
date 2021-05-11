10d8771fe4dfb60550fd4b22eda36c405417d8b3
supermt <supertempler@gmail.com>
2021-05-11 10:22:35 +0900

    add the result of oncpu flamegraph

--- motivation_model/cpu_or_thread/perf_test/plot_flame_graph.sh
+++ motivation_model/cpu_or_thread/perf_test/plot_flame_graph.sh
@@ -1,3 +1,7 @@
-perf script -i perf.data | /home/supermt/git/FlameGraph/stackcollapse-perf.pl > out.perf-folded
-/home/supermt/git/FlameGraph/flamegraph.pl out.perf-folded > perf-kernel-nvme.svg
+for media in nvme pm ssd hdd
+do
+ 
+    perf script -i perf_results/oncpu_perf_$media.data | /home/supermt/git/FlameGraph/stackcollapse-perf.pl > $media.out.perf-folded
+    /home/supermt/git/FlameGraph/flamegraph.pl $media.out.perf-folded > perf-kernel-$media.svg
 
+done
