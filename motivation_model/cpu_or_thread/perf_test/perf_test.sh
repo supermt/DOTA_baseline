82007ff2a8180290f5a3cf847190ceb6b20e2404
supermt <supertempler@gmail.com>
2021-05-21 18:05:36 +0900

    add the results svg of each runs

--- motivation_model/cpu_or_thread/perf_test/perf_test.sh
+++ motivation_model/cpu_or_thread/perf_test/perf_test.sh
@@ -1,18 +1,24 @@
 running_time=2000
-for media in ssd hdd
+for media in nvme pm ssd hdd
 do
     echo "In: $media, run for $running_time, clear the cache"
     sync; echo 1 > /proc/sys/vm/drop_caches
 # Collect On CPU Results for all media
-    perf record -a -g -F 99 -o oncpu_perf_$media.data -- /home/supermt/git/DOTA_Rocksdb/db_bench --db=/home/supermt/rocksdb_$media --benchmarks=fillrandom,stats --num=483183820 --key_size=8 --value_size=100 --write_buffer_size=67108864 --target_file_size_base=67108864 --min_write_buffer_number_to_merge=1 --max_write_buffer_number=2 --level0_file_num_compaction_trigger=4 --max_background_compactions=12 --max_background_flushes=1 --threads=1 --bloom_bits=10 --compression_type=none --base_background_compactions=1 --report_bg_io_stats=False --subcompactions=1 --detailed_running_stats=True --report_interval_seconds=1 --mutable_compaction_thread_prior=false --statistics=true --duration=$running_time --report_file=/home/supermt/rocksdb_$media/report.csv --max_background_jobs=13 > $media.stdout
+#   perf record -a -g -F 99 -o oncpu_perf_$media.data -- /home/supermt/git/DOTA_Rocksdb/db_bench --db=/home/supermt/rocksdb_$media --benchmarks=fillrandom,stats --num=483183820 --key_size=8 --value_size=100 --write_buffer_size=67108864 --target_file_size_base=67108864 --min_write_buffer_number_to_merge=1 --max_write_buffer_number=2 --level0_file_num_compaction_trigger=4 --max_background_compactions=12 --max_background_flushes=1 --threads=1 --bloom_bits=10 --compression_type=none --base_background_compactions=1 --report_bg_io_stats=False --subcompactions=1 --detailed_running_stats=True --report_interval_seconds=1 --mutable_compaction_thread_prior=false --statistics=true --duration=$running_time --report_file=/home/supermt/rocksdb_$media/report.csv --max_background_jobs=13 > $media.stdout
 
 # Collect Off CPU Results for all media
 #    sync; echo 1 > /proc/sys/vm/drop_caches
 #    perf record -a -g -e sched:sched_stat_sleep -e sched:sched_switch -e sched:sched_process_exit -o offcpu_perf_$media.data -- /home/supermt/git/DOTA_Rocksdb/db_bench --db=/home/supermt/rocksdb_$media --benchmarks=fillrandom,stats --num=483183820 --key_size=8 --value_size=100 --write_buffer_size=67108864 --target_file_size_base=67108864 --min_write_buffer_number_to_merge=1 --max_write_buffer_number=2 --level0_file_num_compaction_trigger=4 --max_background_compactions=12 --max_background_flushes=1 --threads=1 --bloom_bits=10 --compression_type=none --base_background_compactions=1 --report_bg_io_stats=False --subcompactions=1 --detailed_running_stats=True --report_interval_seconds=1 --mutable_compaction_thread_prior=false --statistics=true --duration=$running_time --report_file=/home/supermt/rocksdb_$media/report.csv --max_background_jobs=13 > $media.stdout
 
+# use the tool offcputime-bpfcc to analyze the problem 
+    sync; echo 1 > /proc/sys/vm/drop_caches
+    nohup /home/supermt/git/DOTA_Rocksdb/db_bench --db=/home/supermt/rocksdb_$media --benchmarks=fillrandom,stats --num=483183820 --key_size=8 --value_size=100 --write_buffer_size=67108864 --target_file_size_base=67108864 --min_write_buffer_number_to_merge=1 --max_write_buffer_number=2 --level0_file_num_compaction_trigger=4 --max_background_compactions=12 --max_background_flushes=1 --threads=1 --bloom_bits=10 --compression_type=none --base_background_compactions=1 --report_bg_io_stats=False --subcompactions=1 --detailed_running_stats=True --report_interval_seconds=1 --mutable_compaction_thread_prior=false --statistics=true --duration=$running_time --report_file=/home/supermt/rocksdb_$media/report.csv --max_background_jobs=13 > $media.stdout &
+    offcputime-bpfcc --stack-storage-size=2048 -df -p `pgrep -nx db_bench` $running_time > offline_$media.out.stacks
+    sleep 5
     mv /home/supermt/rocksdb_$media/report.csv ./db_bench_results/report_$media.csv
     mv $media.stdout ./db_bench_results/
     mv perf_$media.data ./perf_results/
+    mv offline_$media.out.stacks ./perf_results/
 
 done
 
--- motivation_model/cpu_or_thread/perf_test/plot_flame_graph.sh
+++ motivation_model/cpu_or_thread/perf_test/plot_flame_graph.sh
@@ -1,7 +1,7 @@
 for media in nvme pm ssd hdd
 do
  
-    perf script -i perf_results/oncpu_perf_$media.data | /home/supermt/git/FlameGraph/stackcollapse-perf.pl > $media.out.perf-folded
+    perf script -i perf_results/oncpu_perf_$media.data | grep rocksdb | /home/supermt/git/FlameGraph/stackcollapse-perf.pl > $media.out.perf-folded
     /home/supermt/git/FlameGraph/flamegraph.pl $media.out.perf-folded > perf-kernel-$media.svg
-
+    /home/supermt/git/FlameGraph/flamegraph.pl --color=io --title="Off-CPU Time Flame Graph" --countname=us < perf_results/offline_$media.out.stacks > offcpu-perf-$media.svg
 done
