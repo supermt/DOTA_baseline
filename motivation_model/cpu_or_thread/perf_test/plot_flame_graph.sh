5973281d9ff4041c46a8edfa67d5b7f7b92dbbf6
supermt <supertempler@gmail.com>
2021-05-26 08:31:09 +0900

    add the results of different threads execution

--- motivation_model/cpu_or_thread/perf_test/plot_flame_graph.sh
+++ motivation_model/cpu_or_thread/perf_test/plot_flame_graph.sh
@@ -1,7 +1,13 @@
-for media in nvme pm ssd hdd
-do
- 
-    perf script -i perf_results/oncpu_perf_$media.data | grep rocksdb | /home/supermt/git/FlameGraph/stackcollapse-perf.pl > $media.out.perf-folded
-    /home/supermt/git/FlameGraph/flamegraph.pl $media.out.perf-folded > perf-kernel-$media.svg
-    /home/supermt/git/FlameGraph/flamegraph.pl --color=io --title="Off-CPU Time Flame Graph" --countname=us < perf_results/offline_$media.out.stacks > offcpu-perf-$media.svg
+for cpu_num in 1 4 8
+	do
+        cd $cpu_num/
+        for media in nvme pm ssd hdd
+            do 
+                perf script -i perf_results/oncpu_perf_$media.data | /home/supermt/git/FlameGraph/stackcollapse-perf.pl > $media.out.perf-folded
+                /home/supermt/git/FlameGraph/flamegraph.pl $media.out.perf-folded > perf-kernel-count-$media.svg
+                /home/supermt/git/FlameGraph/flamegraph.pl $media.out.perf-folded > perf-kernel-time_ratio-$media.svg
+                /home/supermt/git/FlameGraph/flamegraph.pl --color=io --title="Off-CPU Time Flame Graph" --countname=us < perf_results/offline_$media.out.stacks > offcpu-perf-$media.svg
+            done
+	chmod 666 *.svg
+        cd ../
 done
