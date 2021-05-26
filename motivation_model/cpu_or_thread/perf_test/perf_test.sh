25c2f7ba311de03f9489369a5838d41d6aaf943a
supermt <supertempler@gmail.com>
2021-05-26 23:56:32 +0900

    add the results of perf on-cpu and offwaketime chaingraph

--- motivation_model/cpu_or_thread/perf_test/perf_test.sh
+++ motivation_model/cpu_or_thread/perf_test/perf_test.sh
@@ -6,10 +6,13 @@ do
 	    echo "In: $media, run for $running_time, clear the cache"
 	# Collect On CPU Results for all medi	# use the tool offcputime-bpfcc to analyze the problem 
 	    sync; echo 1 > /proc/sys/vm/drop_caches
-	    nohup /home/supermt/git/DOTA_Rocksdb/db_bench --db=/home/supermt/rocksdb_$media --benchmarks=fillrandom,stats --num=483183820 --key_size=8 --value_size=100 --write_buffer_size=67108864 --target_file_size_base=67108864 --min_write_buffer_number_to_merge=1 --max_write_buffer_number=2 --level0_file_num_compaction_trigger=4 --max_background_compactions=$cpu_num --max_background_flushes=1 --threads=1 --bloom_bits=10 --compression_type=none --base_background_compactions=1 --report_bg_io_stats=False --subcompactions=1 --detailed_running_stats=True --report_interval_seconds=1 --mutable_compaction_thread_prior=false --statistics=true --duration=$running_time --report_file=/home/supermt/rocksdb_$media/report.csv > $media.stdout 2>$media.stderr &
-	    nohup perf record -a -g -F 99 -o oncpu_perf_$media.data -p `pgrep -nx db_bench` > perf_command.log 2>perf_command.err &
-	    offcputime-bpfcc --stack-storage-size=2048 -df -p `pgrep -nx db_bench` $running_time > offline_$media.out.stacks 
-            sleep 5
+	    nohup perf record -a -g -F 99 -o oncpu_perf_$media.data -- /home/supermt/git/DOTA_Rocksdb/db_bench --db=/home/supermt/rocksdb_$media --benchmarks=fillrandom,stats --num=483183820 --key_size=8 --value_size=100 --write_buffer_size=67108864 --target_file_size_base=67108864 --min_write_buffer_number_to_merge=1 --max_write_buffer_number=2 --level0_file_num_compaction_trigger=4 --max_background_compactions=$cpu_num --max_background_flushes=1 --threads=1 --bloom_bits=10 --compression_type=none --base_background_compactions=1 --report_bg_io_stats=False --subcompactions=1 --detailed_running_stats=True --report_interval_seconds=1 --mutable_compaction_thread_prior=false --statistics=true --duration=$running_time --report_file=/home/supermt/rocksdb_$media/report.csv > $media.stdout 2>$media.stderr &
+	    #nohup perf record -a -g -F 99 -o oncpu_perf_$media.data -p `pgrep -nx db_bench` > perf_command.log 2>perf_command.err &
+	    echo "Perf record and db_bench program started"
+	    sleep 1
+	    offwaketime-bpfcc --stack-storage-size=2048 --pid=`pgrep -nx db_bench` -f $running_time > offline_$media.out.stacks 
+	    echo "offwaketime finished"
+	    sleep 5
 	    mkdir -p $cpu_num/db_bench_results
 	    mkdir -p $cpu_num/perf_results
 	    cd $cpu_num
--- motivation_model/cpu_or_thread/perf_test/plot_flame_graph.sh
+++ motivation_model/cpu_or_thread/perf_test/plot_flame_graph.sh
@@ -1,13 +1,13 @@
 for cpu_num in 1 4 8
-	do
-        cd $cpu_num/
-        for media in nvme pm ssd hdd
-            do 
-                perf script -i perf_results/oncpu_perf_$media.data | /home/supermt/git/FlameGraph/stackcollapse-perf.pl > $media.out.perf-folded
-                /home/supermt/git/FlameGraph/flamegraph.pl $media.out.perf-folded > perf-kernel-count-$media.svg
-                /home/supermt/git/FlameGraph/flamegraph.pl $media.out.perf-folded > perf-kernel-time_ratio-$media.svg
-                /home/supermt/git/FlameGraph/flamegraph.pl --color=io --title="Off-CPU Time Flame Graph" --countname=us < perf_results/offline_$media.out.stacks > offcpu-perf-$media.svg
-            done
-	chmod 666 *.svg
-        cd ../
+do
+cd $cpu_num/
+for media in nvme pm ssd hdd
+    do 
+	perf script -i perf_results/oncpu_perf_$media.data | /home/supermt/git/FlameGraph/stackcollapse-perf.pl > $media.out.perf-folded
+	/home/supermt/git/FlameGraph/flamegraph.pl $media.out.perf-folded > perf-kernel-count-$media.svg
+	/home/supermt/git/FlameGraph/flamegraph.pl $media.out.perf-folded --countname=us> perf-kernel-time_ratio-$media.svg
+	/home/supermt/git/FlameGraph/flamegraph.pl --color=chain --title="Off-CPU Time Flame Graph" --countname=us < perf_results/offline_$media.out.stacks > offcpu-perf-$media.svg
+    done
+chmod 666 *.svg
+cd ../
 done
